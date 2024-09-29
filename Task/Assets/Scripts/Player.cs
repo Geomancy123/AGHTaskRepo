c9dcdbb2afa70e9c6ef66219b8a9cf6887192d2c
George White <tgeorgew465@gmail.com>
2024-09-29 14:33:21 +1300

    Assets+initial player prefab/script

---
+++ Task/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+    [SerializeField] private float fMoveDistance = 1.0f;
+    [SerializeField] private float fMoveDuration = 1.0f;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
